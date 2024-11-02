189b50c59a6249673ffc0c489b4aa891e0cb4454
KimRass <rmx1000@yonsei.ac.kr>
2024-11-02 22:41:24 +0900

    Rule-based weather retrieval

--- ner.py
+++ ner.py
@@ -4,6 +4,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
+import re
 
 
 class NER(object):
@@ -19,3 +20,40 @@ class NER(object):
     @torch.inference_mode()
     def __call__(self, text):
         return self.ner_pipeline(text)
+
+    @staticmethod
+    def parse(ner_out):
+        word = ""
+        words = []
+        for token in ner_out:
+            subword = token["word"]
+            pref, suff = token["entity"].split("-")
+            if suff not in ["DT", "LC"]:
+                continue
+
+            if pref == "I" and words:
+                word, prev_suff = words.pop()
+                if suff != prev_suff:
+                    continue
+
+                if re.match(r"##(?!##)\S+", subword):
+                    word += f"{subword[2:]}"
+                else:
+                    word += f" {subword}"
+            elif pref == "B":
+                word = subword
+            words.append((word, suff))
+
+        dates = []
+        cities = []
+        for word, entity in words:
+            if entity == "DT":
+                dates.append(word)
+            else:
+                cities.append(word)
+        return dates, cities
+
+
+if __name__ == "__main__":
+    ner = NER()
+    ner("내일과 모레 시드니 날씨 말해 줘.")
